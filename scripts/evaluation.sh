b6422039b858294421181320c6b9210929ceeda1
AGMoller <andersgiovanni@gmail.com>
2024-01-08 16:08:56 +0100

    Scripts

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=evaluation    # Job name
 #SBATCH --output=run_outputs/evaluation.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
-#SBATCH --time=18:00:00          # Run time (hh:mm:ss) - run for one hour max
-#SBATCH --gres=gpu:v100:4
+#SBATCH --time=10:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu:a100_40gb:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
--- scripts/sft.sh
+++ scripts/sft.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=run_outputs/sft.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
 #SBATCH --time=24:00:00          # Run time (hh:mm:ss) - run for one hour max
-#SBATCH --gres=gpu:v100:4
+#SBATCH --gres=gpu:a100_40gb:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
