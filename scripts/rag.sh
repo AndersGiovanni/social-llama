2891e56af11367a5e9ba1deb144bddd763851176
Anders Giovanni Møller <andersgiovanni@gmail.com>
2024-02-01 12:16:21 +0100

    RAG Runner

---
+++ scripts/rag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=rag    # Job name
+#SBATCH --output=run_outputs/rag.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --cpus-per-task=12       # Schedule one core
+#SBATCH --time=12:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu:1
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
+#SBATCH --account=researchers
+
+hostname
+nvidia-smi
+python -m src.social_llama.training.rag
