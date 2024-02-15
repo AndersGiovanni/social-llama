d764a2d04eb8be930130251d92484cf0e16e3b10
AGMoller <andersgiovanni@gmail.com>
2024-02-15 11:13:36 +0100

    Update scripts

--- scripts/classifier_multilabel_ovr.sh
+++ scripts/classifier_multilabel_ovr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=classifier_multi    # Job name
+#SBATCH --job-name=classifier_multi_ovr    # Job name
 #SBATCH --output=run_outputs/classifier_multi.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=20       # Schedule one core
-#SBATCH --time=18:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --time=1-12:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --gres=gpu:a100_40gb:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
@@ -13,4 +13,4 @@ hostname
 
 nvidia-smi
 
-python -m src.social_llama.training.classification_news
+python -m src.social_llama.training.classification_news_ovr
