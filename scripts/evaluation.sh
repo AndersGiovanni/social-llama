f18c67b209ed8ce78ead44cb6b3a0f916f6d2018
Anders Giovanni Møller <andersgiovanni@gmail.com>
2024-02-27 15:09:33 +0100

    Zero-shot: Fix runner parameters

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=evaluation    # Job name
 #SBATCH --output=run_outputs/evaluation.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
-#SBATCH --time=12:00:00          # Run time (hh:mm:ss) - run for one hour max
-#SBATCH --gres=gpu:a100_40gb:1
+#SBATCH --time=24:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
