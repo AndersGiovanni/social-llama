227ea593a11d9e3f211d411f244d559b746cbebc
AGMoller <andersgiovanni@gmail.com>
2024-03-04 08:40:38 +0100

    Update runner scripts

---
+++ scripts/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=env    # Job name
+#SBATCH --output=run_outputs/env.%j.out      # Name of output file (%j expands to jobId)
+#SBATCH --cpus-per-task=12       # Schedule one core
+#SBATCH --time=24:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --gres=gpu
+#SBATCH --partition=brown,red    # Run on either the Red or Brown queue
+#SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
+#SBATCH --account=researchers
+
+module load poetry
+poetry shell
+poetry update
+poetry install
--- scripts/rag.sh
+++ scripts/rag.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=rag    # Job name
 #SBATCH --output=run_outputs/rag.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
-#SBATCH --time=24:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --time=2-00:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --gres=gpu:1
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
