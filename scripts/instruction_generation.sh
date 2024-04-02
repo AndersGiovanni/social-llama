8c997dad4dd96fd264cf2ba4d9dca91c0beabf9c
Anders Giovanni Møller <andersgiovanni@gmail.com>
2024-04-02 15:58:30 +0200

    Change runningtime

--- scripts/instruction_generation.sh
+++ scripts/instruction_generation.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=instruction-generation    # Job name
 #SBATCH --output=run_outputs/instruction-generation.%j.out      # Name of output file (%j expands to jobId)
 #SBATCH --cpus-per-task=12       # Schedule one core
-#SBATCH --time=3-00:00:00          # Run time (hh:mm:ss) - run for one hour max
+#SBATCH --time=1-12:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --partition=brown,red    # Run on either the Red or Brown queue
 #SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 #SBATCH --account=researchers
